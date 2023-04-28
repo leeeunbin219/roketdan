234ca6cccab7d5de4a68e946543cef0bc9647cb0
leeeunbin219 <leeeunbin219@gmail.com>
2023-04-28 11:51:20 +0900

    “배포”

--- pages/index.js
+++ pages/index.js
@@ -9,20 +9,20 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="container">
-      <Image src={bgimg} width={1200} className="bgimg" />
+      <Image src={bgimg} width={900} className="bgimg" />
       <div className="img1">
         <Link href={`http://variablemaker.kr/`} passHref>
-          <Image src={roi} height={800} width={400} />
+          <Image src={roi} height={600} width={300} />
         </Link>
       </div>
       <div className="img2">
         <Link href={`https://www.neokkukae.store/`} passHref>
-          <Image src={naong} height={300} width={250} />
+          <Image src={naong} height={200} width={200} />
         </Link>
       </div>
       <div className="img3">
         <Link href={`https://make.mycookie.site/`} passHref>
-          <Image src={rosa} height={800} width={350} />
+          <Image src={rosa} height={600} width={260} />
         </Link>
       </div>
       <style jsx>{home}</style>
@@ -32,7 +32,7 @@ export default function Home() {
 
 const home = css`
   .container {
-    width: 1200px;
+    width: 900px;
     position: absolute;
     top: 50%;
     left: 50%;
@@ -40,17 +40,17 @@ const home = css`
   }
   .img1 {
     position: absolute;
-    top: 100px;
+    top: 60px;
     left: 100px;
   }
   .img2 {
     position: absolute;
-    top: 550px;
-    right: 500px;
+    top: 450px;
+    right: 380px;
   }
   .img3 {
     position: absolute;
-    top: 100px;
+    top: 60px;
     right: 100px;
   }
 `;
