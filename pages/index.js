1e1d7bc63a5a36c94914a68758fc192134329e92
leeeunbin219 <leeeunbin219@gmail.com>
2023-04-28 11:33:19 +0900

    배포

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,56 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import css from "styled-jsx/css";
+import Image from "next/image";
+import naong from "../public/images/naong.png";
+import roi from "../public/images/roi.png";
+import rosa from "../public/images/rosa.png";
+import bgimg from "../public/images/bgimg.jpeg";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+    <div className="container">
+      <Image src={bgimg} width={1200} className="bgimg" />
+      <div className="img1">
+        <Link href={`http://variablemaker.kr/`} passHref>
+          <Image src={roi} height={800} width={400} />
+        </Link>
+      </div>
+      <div className="img2">
+        <Link href={`https://www.neokkukae.store/`} passHref>
+          <Image src={naong} height={300} width={250} />
+        </Link>
+      </div>
+      <div className="img3">
+        <Link href={`https://make.mycookie.site/`} passHref>
+          <Image src={rosa} height={800} width={350} />
+        </Link>
+      </div>
+      <style jsx>{home}</style>
     </div>
-  )
+  );
 }
+
+const home = css`
+  .container {
+    width: 1200px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .img1 {
+    position: absolute;
+    top: 100px;
+    left: 100px;
+  }
+  .img2 {
+    position: absolute;
+    top: 550px;
+    right: 500px;
+  }
+  .img3 {
+    position: absolute;
+    top: 100px;
+    right: 100px;
+  }
+`;
